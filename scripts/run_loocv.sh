6317131c277d19cbfd3e1bbdc698fd8ba5519d9f
MattFeig <30275344+MattFeig@users.noreply.github.com>
2023-02-06 14:22:22 -0800

    forest update

--- scripts/run_loocv.sh
+++ scripts/run_loocv.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=forestLOOCV
-###tempremoveSBATCH --cpus-per-task=8
-##removeSBATCH --mem-per-cpu=10G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mfeigeli@ucsd.edu
+##tempSBATCH --cpus-per-task=8
+##removeSBATCH --mem-per-cpu=16G
+
 
 estimators=$1
 python3 loocv.py "$estimators"
